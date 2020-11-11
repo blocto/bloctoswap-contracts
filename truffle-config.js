d1c06fbc5dc8100a31fb55a45e99e73f2e0f46c6
Hsuan Lee <boczeratul@gmail.com>
2020-11-11 16:18:51 +0800

    Add Flow emulator configs

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,7 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = fs.readFileSync("dev-mnemonic.txt").toString().trim();
 
 module.exports = {
   /**
