ac555014336c794a1ba8b60f68e0d08db3b11cf4
Hsuan Lee <boczeratul@gmail.com>
2021-01-11 12:28:36 +0800

    Add USDC to contract

--- contracts/teleport/ethereum/TeleportCustody.sol
+++ contracts/teleport/ethereum/TeleportCustody.sol
@@ -7,6 +7,10 @@ import "./TeleportAdmin.sol";
 import "./TetherToken.sol";
 
 contract TeleportCustody is TeleportAdmin {
+  // USDC
+  // ERC20 private _tokenContract = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+  
+  // USDT
   TetherToken private _tokenContract = TetherToken(0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
   // Records that an unlock transaction has been executed
--- contracts/teleport/ethereum/TetherToken.sol
+++ contracts/teleport/ethereum/TetherToken.sol
@@ -2,7 +2,11 @@
 
 pragma solidity ^0.6.0;
 
+/**
+ * @dev Method signature contract for Tether (USDT) because it's not a standard
+ * ERC-20 contract and have different method signatures.
+ */
 contract TetherToken {
-    function transfer(address _to, uint _value) public {}
-    function transferFrom(address _from, address _to, uint _value) public {}
+  function transfer(address _to, uint _value) public {}
+  function transferFrom(address _from, address _to, uint _value) public {}
 }
